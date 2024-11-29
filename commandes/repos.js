f025e6b36ca192505f8c2336d8631387d30a2072
YESSER TECH <aidenyassin115@gmail.com>
2024-11-29 22:59:55 +0300

    Update repos.js

--- commandes/repos.js
+++ commandes/repos.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
-zokou({ nomCom: "repo", catégorie:"Général", reaction: "⚙️", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "repo", catégorie:"Général", reaction: "🙄", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
   const githubRepo = 'https://api.github.com/repos/Yassin994/YESSER-MD';
   const img = 'https://files.catbox.moe/44l78z.jpg';
 
@@ -22,22 +22,22 @@ zokou({ nomCom: "repo", catégorie:"Général", reaction: "⚙️", nomFichier: 
       const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
       const gitdata = ` *Hellow User
-This Is* *YESSIR-MD.*\n _Support Our Channel_ *by*,  https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f
+This Is* *YESSIR-MD.*\n _Support Our Channel_ *by*,  https://whatsapp.com/channel/0029VakA1mu35fM18opH1s30
 
 🗼 *REPOSITORY:* ${data.html_url}
 
 🕐 *UPDATE ON:* ${repoInfo.lastUpdate}
 ⊷━━━━━━☆•∞•☆━━━━━━⊷  
 
- ╭━━═✺ *FE255* ✺═━━⊷
+ ╭━━═✺ *𝚈𝙴𝚂𝚂𝙴𝚁* ✺═━━⊷
  │┌═━━⊷•∞•⊷━━─⊛
  │┊❁*VISITORS:* ${repoInfo.visitors}
  │┊❁ *STARS:* ${repoInfo.stars}
  │┊❁ *FORKS:* ${repoInfo.forks}
  │┊❁ *RELEASE DATE:* ${releaseDate}
- │┊❁ *OWNER:* *YAssin tech*
- │┊❁ *THEME:* *FE255*
- │┊❁ *ENJOY TO USE YESSIR MD*
+ │┊❁ *OWNER:* *𝐲𝐞𝐬𝐬𝐞𝐫𝐭𝐞𝐜𝐡*
+ │┊❁ *THEME:* *𝐦𝐲𝐜𝐚𝐫*
+ │┊❁ *𝓌ℯ 𝒶𝓁𝓌𝒶𝓎𝓈 𝓁ℴ𝓋ℯ 𝓎ℴ𝓊❤️*
  │└═━━⊷•∞•⊷━━─⊛
  ╰━━━═⊷✺•∞•✺⊷═━━━⊷ 
  
