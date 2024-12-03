34653be76e6594968bebd04f98c8ea366efe365b
YESSER TECH <aidenyassin115@gmail.com>
2024-12-04 00:34:45 +0300

    Update uptime.js

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -28,7 +28,7 @@ zokou({ nomCom: 'uptime',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
 
-                 await repondre(`*_uptime of YESSER md  bot is: ${runtime(process.uptime())}_*`) 
+                 await repondre(`*_𝙮𝙚𝙨𝙨𝙚𝙧 𝙢𝙙 𝚞𝚙𝚝𝚒𝚖𝚎 𝚒𝚜: ${runtime(process.uptime())}_*`) 
 
    
 
